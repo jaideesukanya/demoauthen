332df0d745d914cbb39138dcb0836eef087ce248
cpongaew <31723033+cpongaew@users.noreply.github.com>
2017-09-29 16:10:21 +0700

    formio

---
+++ config.js
@@ -0,0 +1,22 @@
+var APP_URL = 'https://vlivngtmaqhnaje.form.io';
+var API_URL = 'https://api.form.io';
+
+angular.module('formioApp').constant('AppConfig', {
+  appUrl: APP_URL,
+  apiUrl: API_URL,
+  forms: {
+      userForm: APP_URL + '/tstauthen'
+  }
+  /*forms: {
+    userForm: APP_URL + '/user',
+    userLoginForm: APP_URL + '/user/login',
+    userRegisterForm: APP_URL + '/user/register'
+	
+  },
+  resources: {
+    user: {
+      form: APP_URL + '/user',
+      resource: 'UserResource'
+    }
+  }*/
+});
